baf91d840b3baa592e0ee67a557040f29f6701fe
rubenpiccoli <rubendariopiccoli@gmail.com>
2022-04-26 12:18:54 -0300

    se comienza con el Frond End diseño login con modal y pagina principal

--- frond-end/catalogo-articulos/src/main.js
+++ frond-end/catalogo-articulos/src/main.js
@@ -5,12 +5,5 @@ import store from './store'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {faUserSecret,faCoffee,faArrowsDownToPeople } from '@fortawesome/free-solid-svg-icons'
-import { faGithub  } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faUserSecret,faCoffee,faArrowsDownToPeople,faGithub )
 
-//Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
